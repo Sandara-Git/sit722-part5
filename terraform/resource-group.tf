69ea9a9781e6359ec235d8042cd72991cac553eb
Sandara Wijethunga <140485221+Sandara-Git@users.noreply.github.com>
2024-10-10 00:19:56 +1100

    Update resource-group.tf

--- terraform/resource-group.tf
+++ terraform/resource-group.tf
@@ -2,6 +2,6 @@
 # Creates a resource group for FlixTube in your Azure account.
 #
 resource "azurerm_resource_group" "sit722sandara" {
-  name     = sit722sandara
-  location = australiasoutheast
+  name     = "sit722sandara"
+  location = "australiasoutheast"
 }
